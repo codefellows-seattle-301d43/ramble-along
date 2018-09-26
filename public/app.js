39ee9bc06513ee555647156b1816d01a6517eea4
neth <nethwebdev@gmail.com>
2018-09-25 17:32:24 -0700

    styled new form, new form adds new happening

--- public/app.js
+++ public/app.js
@@ -1,21 +1,39 @@
 'use strict';
 
-// $( document ).ready(function() {
+// Check local storage and populate myHapsIds array
+let myHapsIds = [];
+function populateMyHaps() {
+  if (localStorage.getItem('myHapsArr')) {
+    myHapsIds = JSON.parse(localStorage.getItem('myHapsArr'));
+  }
+}
+populateMyHaps();
 
-  $( '.cross' ).hide();
-  $( '.menu' ).hide();
-  $( '.hamburger' ).on('click', function() {
-    $( '.menu' ).slideToggle( 'slow', function() {
-      $( '.hamburger' ).hide();
-      $( '.cross' ).show();
-    });
+// Hamburger Menu Controlls for nav.ejs
+$( '.cross' ).hide();
+$( '.menu' ).hide();
+$( '.hamburger' ).on('click', function() {
+  $( '.menu' ).slideToggle( 'slow', function() {
+    $( '.hamburger' ).hide();
+    $( '.cross' ).show();
   });
+});
 
-  $( '.cross' ).on('click', function() {
-    $( '.menu' ).slideToggle( 'slow', function() {
-      $( '.cross' ).hide();
-      $( '.hamburger' ).show();
-    });
+$( '.cross' ).on('click', function() {
+  $( '.menu' ).slideToggle( 'slow', function() {
+    $( '.cross' ).hide();
+    $( '.hamburger' ).show();
   });
+});
 
-//});
+// Function that fires on loading my-happenings.ejs
+function pushToMyHaps(id) {
+  myHapsIds.push(id);
+  const myHapsStr = JSON.stringify(myHapsIds);
+  localsStorage.setItem('myHapsArr', myHapsStr);
+};
+
+// Function that populates hidden inputs with id numbers
+// function populateHiddenInput() {
+  
+// }
